af7989633ad336f78fd07a3dc0b5040cb9227162
Alberttron005 <129827506+Alberttron005@users.noreply.github.com>
2023-04-04 03:40:24 -0700

    Update fbg.lua

--- fbg.lua
+++ fbg.lua
@@ -14,6 +14,16 @@ local FarmingSection = GeneralTab:CreateSection({
 Name = "Leveling"
 })
 
+FarmingSection:AddButton({
+Name = "Tp To afkspot",
+Callback = function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(math.random(8999990000, 8999999999), 8999999488, math.random(8999990000, 8999999999))
+end})
+
+
+
+
+
 
 FarmingSection:AddToggle({
 Name = "TSRubber Farm",
@@ -30,6 +40,7 @@ end
 end
 end)
 
+
 FarmingSection:AddToggle({
 Name = "Dough Farm",
 Flag = "Dough"
@@ -37,8 +48,8 @@ Flag = "Dough"
 spawn(function()
     while wait(0.5) do
         if library.flags["Dough"] then 
-game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","Grilled Dough",{})
-game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","Dough Muddle",{})
+game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","GrilledDough",{})
+game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","DoughMuddle",{})
 game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","Chestnut",{})
 game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dough","Buzzcut",{})
         end
@@ -55,8 +66,16 @@ spawn(function()
 game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","Screech",{})
 game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","DragonClaw",{})
 game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","Screech",{})
-game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","DragonMeteor",{})
-game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","CrimsonBomb",{}) 
+game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","DracoMeteor",{})
+game:GetService("ReplicatedStorage").ReplicatorNoYield:FireServer("Dragon","CrimsonBomb", {
+["MouseRay"] = {
+["Normal"] = Vector3.new(0, 1, 3.505146537463588e-07),
+["Direction"] = Vector3.new(90.00456237792969, -134.73870849609375, 252.47703552246094),
+["Origin"] = Vector3.new(-432.52044677734375, 761.1904907226562, 359.0203552246094),
+["Instance"] = workspace.SafeZones.FlowerHill.MainColor,
+["Distance"] = 41.85980224609375,
+["Position"] = Vector3.new(-419.9618835449219, 742.3900146484375, 394.2491455078125),["Material"] = Enum.Material.Slate
+}})
         end
     end
 end)
