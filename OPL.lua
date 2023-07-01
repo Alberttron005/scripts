794a492817f80956c6a00fbf2d940ce9362c99e2
Alberttron005 <129827506+Alberttron005@users.noreply.github.com>
2023-07-01 04:26:11 -0700

    Create OPL.lua

---
+++ OPL.lua
@@ -0,0 +1,85 @@
+local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+local Wait = library.subs.Wait -- Only returns if the GUI has not been terminated. For 'while Wait() do' loops
+
+local PepsisWorld = library:CreateWindow({
+Name = "idk",
+})
+local GeneralTab = PepsisWorld:CreateTab({
+Name = "idk"
+})
+
+local idk = GeneralTab:CreateSection({
+Name = "General"
+})
+
+idk:AddToggle({
+Name = "auto tp",
+Flag = "Autode"
+})
+spawn(function()
+    while wait(0.1) do
+        if library.flags["Autode"] then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(197793.969, 20007.1035, 192644.969)
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.name == "Cannon Ball" then
+            v.Parent = game.Players.LocalPlayer.Character
+        
+end
+end
+end
+end
+end)
+
+
+idk:AddToggle({
+Name = "Tp mobs",
+Flag = "Autodel"
+})
+spawn(function()
+    while wait(0) do
+        if library.flags["Autodel"] then 
+for i,v in pairs(game.Workspace.Enemies:GetChildren()) do
+if v:isA("Model") then 
+v.HumanoidRootPart.CanCollide = false
+v.HumanoidRootPart.Anchored = true
+v.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position + Vector3.new(0, 0, 15))
+end
+end
+end
+end
+end)
+
+
+idk:AddToggle({
+Name = "Kill mobs",
+Flag = "Autod"
+})
+spawn(function()
+    while wait(0.1) do
+        if library.flags["Autod"] then
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.Character:GetChildren()) do
+if v.Name == "Cannon Ball" then
+v.RemoteEvent:FireServer(CFrame.new(197793.96875, 20007.126953125, 192658.96875) * CFrame.Angles(-3.0365123748779297, -0, -3.1415927410125732))
+end
+
+end
+end
+end
+end)
+
+
+--anti afk
+for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+   v:Disable()
+end
+
+local Part = Instance.new("Part")
+Part.Anchored = true
+Part.BottomSurface = Enum.SurfaceType.Smooth
+Part.TopSurface = Enum.SurfaceType.Smooth
+Part.Color = Color3.fromRGB(0, 0, 0)
+Part.Material = Enum.Material.Plastic
+Part.Size = Vector3.new(15, 1, 15)
+Part.CFrame = CFrame.new(197793.969, 20003.6099, 192644.969)
+Part.Parent = workspace
